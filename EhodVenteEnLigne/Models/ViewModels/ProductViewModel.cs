ed6b14e5065218904502b4aeef2bb5e90420d27c
BenjiBihop <benjamindalyndiaye@houdatech.com>
2024-04-16 00:21:13 +0000

    resolution des erreurs au niveau des champs du formulaire dans le produit et la commande

---
+++ EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EhodBoutiqueEnLigne.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "MissingName")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "MissingStock")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "StockNotAnInteger")]
+        public string Stock { get; set; }
+
+        [Required(ErrorMessage = "MissingPrice")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "PriceNotANumber")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
+        public string Price { get; set; }
+    }
+}
